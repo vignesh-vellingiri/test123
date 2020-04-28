87175fef8f83b557be0e816e21e49b17e2a05769
vignesh <vignesh.vellingiri@aot-technologies.com>
2020-04-27 23:51:57 -0700

    a

--- src/main/java/com/aot/invoice/properties/InvoiceConstants.java
+++ src/main/java/com/aot/invoice/properties/InvoiceConstants.java
@@ -2,5 +2,5 @@ package com.aot.invoice.properties;
 
 public class InvoiceConstants {
     public final static String invoiceInboundPath = "/app/apps/AOTVendorInvoicing/inbound/";
-    public final static String invoiceOutboundPath = "/app/apps/AOTVendorInvoicing/inbound/";
+    public final static String invoiceOutboundPath = "/app/apps/AOTVendorInvoicing/outbound/";
 }
