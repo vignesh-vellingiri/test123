4de51ee97445ecc2b49691e0ac78e79fb9845aac
vignesh <vignesh.vellingiri@aot-technologies.com>
2020-04-28 00:40:59 -0700

    t

--- src/main/java/com/aot/invoice/topic/InvoiceSender.java
+++ src/main/java/com/aot/invoice/topic/InvoiceSender.java
@@ -3,6 +3,7 @@ package com.aot.invoice.topic;
 import com.aot.invoice.model.Invoice;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -11,7 +12,7 @@ import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class InvoiceSender {
-    private final static String TOPIC = "InvoiceTopic";
+    private final static String TOPIC ="InvoiceTopic";
     private final static String BOOTSTRAP_SERVER = "kafka:9092";
     private static Producer<String, String> producer;
 
@@ -22,21 +23,27 @@ public class InvoiceSender {
     private static Producer<String, String> getProducer() {
         Properties settings = new Properties();
         settings.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
-        settings.put(ProducerConfig.CLIENT_ID_CONFIG, "OrderProducerGroup");
+        settings.put(ProducerConfig.CLIENT_ID_CONFIG, "com.aot.orders.topic.OrderStatusProducer");
         settings.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         settings.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        settings.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        settings.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
         settings.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/app/apps/AOTVendorInvoicing/src/main/java/com/aot/invoice/jks/kafka.truststore.jks");
-        settings.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "test1234");
+        settings.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "test123");
         settings.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/app/apps/AOTVendorInvoicing/src/main/java/com/aot/invoice/jks/kafka.keystore.jks");
-        settings.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "test1234");
-        settings.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "test1234");
-        return new KafkaProducer<>(settings);
+        settings.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "test123");
+        settings.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "test123");
+        settings.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        settings.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"" + "user" + "\" password=\"" + "password" + "\";");
+        KafkaProducer<String, String> t = new KafkaProducer<String, String>(settings);
+        System.out.println("after getProdcuer");
+        //return new KafkaProducer<>(settings);
+        return t;
     }
 
     public static void sendToPayment(final String status, final Invoice invoice) throws ExecutionException, InterruptedException {
         System.out.println("Invoice - " + invoice.toString());
         producer= getProducer();
+        System.out.println("after getProdcuer");
         long time = System.currentTimeMillis();
         try {
                 final ProducerRecord<String, String> record =
